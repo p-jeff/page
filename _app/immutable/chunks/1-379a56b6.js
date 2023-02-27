d2f804f3bf3eafb0bd0d98505df56486128940e1
Johannes <lotzejohannes@gmail.com>
2023-02-27 15:23:19 +0100

    Updates

--- _app/immutable/chunks/1-379a56b6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-83bd115e.js";export{t as component};
--- _app/immutable/chunks/2-e6591c84.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-92f56dfd.js";export{t as component};
